274a7641e2448ce5fe35b86cdece1ebded723986
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-01-30 11:20:59 -0300

    :tada: commit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
