e26abd16297e0c27b9ac36caed3b2c8d49305488
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2025-03-05 11:07:26 -0300

    :bug: fix: ajustes no swagger na entidade usuario

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -13,7 +13,9 @@ import {
 import { Usuario } from '../entities/usuario.entity';
 import { UsuarioService } from '../services/usuario.service';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Usuario')
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -8,45 +8,56 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { Viagem } from '../../viagem/entities/viagem.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_usuarios' })
 export class Usuario {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column({ length: 14, nullable: false })
   @IsNotEmpty()
+  @ApiProperty()
   cpf: string;
 
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   nome: string;
 
   @IsNotEmpty()
   @Column({ type: 'date', nullable: false })
+  @ApiProperty()
   data_nascimento: Date;
 
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   tipo_usuario: string;
 
   @Column({ length: 14, nullable: false })
   @IsNotEmpty()
+  @ApiProperty()
   telefone: string;
 
   @IsEmail()
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty({example: 'email@email.com'})
   usuario: string;
 
   @MinLength(8)
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   senha: string;
 
   @Column({ length: 5000 })
+  @ApiProperty()
   foto: string;
 
+  @ApiProperty({type: () => Viagem})
   @OneToMany(() => Viagem, (viagem) => viagem.usuario)
   viagem: Viagem[];
 
